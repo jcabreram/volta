b78b81a25f7e6273d5cf43caf8a48f781017fdf0
Jonathan Cabrera <jcabrera.moreno@gmail.com>
2016-11-15 17:02:39 -0600

    Added timesheets main views and corresponding classes

---
+++ Volta/DaysTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DaysTableViewController.h
+//  Volta
+//
+//  Created by Jonathan Cabrera on 11/15/16.
+//  Copyright © 2016 Ksquare Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DaysTableViewController : UITableViewController
+
+@end
---
+++ Volta/WeekCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WeekCollectionViewCell.h
+//  Volta
+//
+//  Created by Jonathan Cabrera on 11/15/16.
+//  Copyright © 2016 Ksquare Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeekCollectionViewCell : UICollectionViewCell
+
+@end
